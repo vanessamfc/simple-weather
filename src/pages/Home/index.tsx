cfd1aaabb7304bc9598325bd789717444182ebdc
Vanessa Maria <vanessamfc@outlook.com>
2020-07-01 14:53:31 -0300

    add celcius degree

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,38 +1,39 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 const Home: React.FC = () => {
   const [search, setSearch] = useState("");
-  const [city, setCity] = useState("");
-
+  const [city, setCity] = useState();
   const [country, setCountry] = useState("");
   const [feelsLike, setFeelsLike] = useState("");
   const [temp, setTemp] = useState("");
   const [tempMax, setTempMax] = useState("");
   const [tempMin, setTempMin] = useState("");
+  const [wheatherDescription, setWheatherDescription] = useState("");
 
   async function handleSubmit() {
     const {
       data: {
         main: { feels_like, temp, temp_max, temp_min },
         sys: { country },
+        weather: [{ main, description }],
         name,
       },
-    } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=9294b45a8bfba854cc201f95aef3c1af
+    } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=9294b45a8bfba854cc201f95aef3c1af
     `);
-    const celsiusTemp = (feels_like - 273.15).toFixed(1);
-    setFeelsLike(celsiusTemp);
+
     setCity(name);
     setCountry(country);
     setTemp(temp);
     setTempMax(temp_max);
     setTempMin(temp_min);
+    setWheatherDescription(description);
   }
 
   return (
-    <>
+    <Container>
       <div>
         <input
           type="text"
@@ -46,11 +47,18 @@ const Home: React.FC = () => {
         </button>
       </div>
       <div>
-        <h1>feels like:{feelsLike}</h1>
-        <span>{city}</span>
-        <span>{temp} </span>
+        <h2>{wheatherDescription}</h2>
+        {temp ? <p>{temp}°</p> : <p></p>}
+        {tempMin && tempMax && (
+          <span>
+            {tempMin}° / {tempMax}°
+          </span>
+        )}
+        <span>
+          {city},{country}
+        </span>
       </div>
-    </>
+    </Container>
   );
 };
 
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to right, #c2e59c, #64b3f4);
+`;
